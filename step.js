8cebfb6d6d956739973b0d5d1ba7f03a669864ce
Your Name <you@example.com>
2022-11-05 15:03:46 +0600

    about section er basic part done

---
+++ step.js
@@ -0,0 +1,13 @@
+/**
+ * ======== This is about section ============
+ * 1. section er jonno html ke structure crete kora
+ * 2. section er padding top bottom deya
+ * 3. initial flex er ektah value deya
+ * 4. section er title ke style deya postion rela
+ * 5. title eh after before use kora
+ * 6. section title er nice ar ektah row neya
+ * 7. about content er text ke style
+ * 8. h3 ke style span ke color
+ * 9. p er line hight color
+ * 10. second row er niche ar ektah row niey tateh skill add kora
+ */
