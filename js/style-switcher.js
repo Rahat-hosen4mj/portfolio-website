a2cc59d92061126940a8cadcb90f5b46199a5d3d
Your Name <you@example.com>
2022-11-15 00:04:09 +0600

    active themecolor set done with help of javascript

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -2,5 +2,40 @@
 const styleSwitcherToggle = document.querySelector('.style_switcher_toggler');
 styleSwitcherToggle.addEventListener('click', () =>{
     document.querySelector('.style_switcher').classList.toggle('open')
-    console.log('toggle btn clicked')
+    // console.log('toggle btn clicked')
 })
+// hide style switcher on scrool
+window.addEventListener("scroll", () =>{
+    if(document.querySelector('.style_switcher').classList.contains('open')){
+        document.querySelector('.style_switcher').classList.remove('open')
+    }
+});
+
+// theme color 
+const alternateStyle = document.querySelectorAll('.alternate-style');
+function setActiveStyle(color){
+    alternateStyle.forEach((style) =>{
+        if(color === style.getAttribute('title')){
+            style.removeAttribute('disabled')
+        }
+        else{
+            style.setAttribute('disabled', "true")
+        }
+    })
+}
+
+
+
+
+
+// const alternateStyle = document.querySelectorAll('.alternate-style');
+// function setActiveStyle(color){
+//     alternateStyle.forEach((style) =>{
+//         if(color === style.getAttribute('title')){
+//             style.removeAttribute('disabled')
+//         }
+//         else{
+//             style.setAttribute('disabled', "true")
+//         }
+//     })
+// }
