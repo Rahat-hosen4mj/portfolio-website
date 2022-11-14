16b2165cd56e7de23ad72ecd9613d76182452f75
Your Name <you@example.com>
2022-11-15 00:22:38 +0600

    sun and moon toggle hocce theme color change er jonno

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -24,18 +24,17 @@ function setActiveStyle(color){
     })
 }
 
-
-
-
-
-// const alternateStyle = document.querySelectorAll('.alternate-style');
-// function setActiveStyle(color){
-//     alternateStyle.forEach((style) =>{
-//         if(color === style.getAttribute('title')){
-//             style.removeAttribute('disabled')
-//         }
-//         else{
-//             style.setAttribute('disabled', "true")
-//         }
-//     })
-// }
+// theme light and dark
+const dayNight = document.querySelector('.day_night')
+dayNight.addEventListener('click', ()=>{
+    dayNight.querySelector('i').classList.toggle('fa-sun')
+    dayNight.querySelector('i').classList.toggle('fa-moon')
+    document.body.classList.toggle('dark')
+});
+window.addEventListener("load", () =>{
+    if(document.body.classList.contains('dark')){
+        dayNight.querySelector('i').classList.add('fa-sun')
+    }else{
+        dayNight.querySelector('i').classList.add('fa-moon')
+    }
+})
