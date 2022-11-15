54ba42619b170f8c6a7627952966f04b9c03741e
Your Name <you@example.com>
2022-11-15 17:14:53 +0600

    hire me btn woking it nevigate to contact me page

--- js/script.js
+++ js/script.js
@@ -5,6 +5,31 @@ var typed = new Typed(".typing",{
     BackSpeed: 50,
     loop: true
 });
+
+// ================ aside nav color =================
+// const nav = document.querySelector('.nav')
+// const navList = document.querySelectorAll('li')
+// const totalNav = navList.length;
+// for(let i=0; i<totalNav; i++){
+//     // console.log(navList[i])
+//     const a = navList[i].querySelector('a');
+//     a.addEventListener('click', function(){
+//         // console.log(this)
+//         for(let j = 0; j<totalNav; j++){
+//             navList[j].querySelector('a').classList.remove('active')
+//         }
+//         this.classList.add('active')
+//         showSection(this)
+//     })
+// }
+// function showSection(element){
+//     // console.log(element.getAttribute('href').split('#')[1])
+//     for(let i=1; i<totalSection; i++){
+//         allSection[i].classList.remove('active');
+//     }
+//     const target = element.getAttribute('href').split('#')[1];
+//     document.querySelector('#' + target).classList.add('active')
+// }
 // ================ aside nav color =================
 const nav = document.querySelector('.nav')
 const navList = document.querySelectorAll('li'),
@@ -27,7 +52,10 @@ for(let i=0; i<totalNav; i++){
             navList[j].querySelector('a').classList.remove('active')
         }
         this.classList.add('active');
-        showSection(this)
+        showSection(this);
+        if(window.innerWidth < 1200){
+            asideSectionTogglerBtn()
+        }
     })
 }
 function showSection(element){
@@ -38,6 +66,18 @@ function showSection(element){
     const target = element.getAttribute('href').split('#')[1];
     document.querySelector('#' + target).classList.add('active')
 }
+function updateNav(element){
+    // console.log( element.getAttribute('href').split('#')[1])
+    for(let i=0; i<totalNav; i++){
+        navList[i].querySelector('a').classList.remove("active");
+        const target = element.getAttribute('href').split("#")[1]
+    }
+}
+document.querySelector('.hire_me').addEventListener('click', function(){
+    showSection(this)
+    updateNav(this)
+//    console.log(this)
+})
 const navToggleBtn = document.querySelector(".nav_toggler"),
    aside = document.querySelector(".aside");
    navToggleBtn.addEventListener('click', () =>{
